160b0911ac6b5718b993016b67b2f4dbb56ca34c
birchtree2 <1980878786@qq.com>
2024-11-27 15:55:55 +0800

    4.6调整时钟中断

--- src/lab4/init/main.c
+++ src/lab4/init/main.c
@@ -1,7 +1,7 @@
 #include "printk.h"
 #include "defs.h"
 extern void test();
-
+extern void schedule();
 int start_kernel() {
     printk("2024");
     printk(" ZJU Operating System\n");
@@ -11,6 +11,7 @@ int start_kernel() {
     // // printk("old sscrtach: %llx\n", csr_read(sscratch));
     // csr_write(sscratch, 0x1000);
     // printk("new sscrtach: %llx\n", csr_read(sscratch));
+    schedule(); //现在更改为 OS boot 完成之后立即调度 uapp 运行
     test();
     return 0;
 }
