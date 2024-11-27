a13058ae13725b8a1e547acd839b1c90045c06b3
birchtree2 <1980878786@qq.com>
2024-11-27 15:46:37 +0800

    4.4 更改中断处理逻辑

--- src/lab4/arch/riscv/kernel/trap.c
+++ src/lab4/arch/riscv/kernel/trap.c
@@ -1,8 +1,19 @@
 #include "stdint.h"
 #include "printk.h"
 #include "clock.h"
+#include "syscall.h"
 extern void do_timer();
-void trap_handler(uint64_t scause, uint64_t sepc) {
+struct pt_regs {
+    uint64_t ra;
+    uint64_t norm[29]; //x3-x31 32个寄存器去掉了x0,ra,sp
+    uint64_t scause;
+    uint64_t sepc;
+    uint64_t sp;//sp在下面 见entry.S
+};
+#define LOAD_PAGE_FAULT 13
+#define ECALL_FROM_U_MODE 8
+#define ECALL_FROM_S_MODE 9
+void trap_handler(uint64_t scause, uint64_t sepc,struct pt_regs *regs) {
     // 通过 `scause` 判断 trap 类型
     // 如果是 interrupt 判断是否是 timer interrupt
     // 如果是 timer interrupt 则打印输出相关信息，并通过 `clock_set_next_event()` 设置下一次时钟中断
@@ -25,5 +36,16 @@ void trap_handler(uint64_t scause, uint64_t sepc) {
     }
     else{//为0则是exception
         printk("exception,code=%x\n",scause);
+        if(scause==ECALL_FROM_U_MODE){
+            printk("ecall from user mode\n");
+            //x17(a7)存放了系统调用号  对应norm[14]
+            //x10(a0)存放了参数1  对应norm[7]
+            if(regs->norm[14]==SYS_WRITE){
+                regs->norm[7]=write(regs->norm[7],(const char*)regs->norm[8],regs->norm[9]);   
+            }else if(regs->norm[14]==SYS_GETPID){
+                regs->norm[7]=getpid();
+            }
+            regs->sepc+=4;//跳过ecall指令
+        }
     }
 }
