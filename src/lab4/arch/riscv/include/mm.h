be54f2c0dd342bae4797b1102708416e4bb8b936
birchtree2 <1980878786@qq.com>
2024-11-28 10:47:49 +0800

    同步mm.c和makefile

--- src/lab4/arch/riscv/include/mm.h
+++ src/lab4/arch/riscv/include/mm.h
@@ -12,4 +12,17 @@ void mm_init();
 void *kalloc();
 void kfree(void *);
 
+struct buddy {
+  uint64_t size;
+  uint64_t *bitmap; 
+};
+
+void buddy_init();
+uint64_t buddy_alloc(uint64_t);
+void buddy_free(uint64_t);
+
+void *alloc_pages(uint64_t);
+void *alloc_page();
+void free_pages(void *);
+
 #endif
